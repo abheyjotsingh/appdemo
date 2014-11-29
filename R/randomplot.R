9545fd71e9fa73a0d55ef651339ddb70d0cec5cc
abheyjotsingh <abheyjot_singh@yahoo.com>
2014-11-29 08:04:12 -0500

    updates
    updates by Abhey

--- R/randomplot.R
+++ R/randomplot.R
@@ -10,14 +10,42 @@ randomplot <- function(n, dist=c("normal", "uniform")){
   dist <- match.arg(dist)
   stopifnot(n < 1e6)
   
-  if(dist == "normal"){
-    hist(rnorm(n))
-  }
+#  if(dist == "normal"){
+#    hist(rnorm(n))
+#  }
   
-  if(dist == "uniform"){
-    hist(runif(n))
-  }
+#  if(dist == "uniform"){
+#    hist(runif(n))
+#  }
+  library(quantstrat)
+  startDate <- '2010-01-01'  # start of data
+  endDate <-  '2014-07-31'   # end of data
+  symbols = c("ITOT", "AGG", "GLD", "VNQ")
+  Sys.setenv(TZ="UTC")       # set time zone
   
+  ## ----results='hide'------------------------------------------------------
+  getSymbols(symbols, src='yahoo', index.class=c("POSIXt","POSIXct"),
+             from=startDate, to=endDate,adjust=TRUE)
+  
+  ## ----results='hide'------------------------------------------------------
+  initDate <- '2009-12-31'
+  initEq <- 1e6
+  currency("USD")
+  stock(symbols, currency="USD",multiplier=1)
+  
+  
+  ## ------------------------------------------------------------------------
+  myTheme<-chart_theme()
+  myTheme$col$dn.col<-'lightblue'
+  myTheme$col$dn.border <- 'lightgray'
+  myTheme$col$up.border <- 'lightgray'
+  
+  ## ----XLX3x3,cache=FALSE--------------------------------------------------
+  #par(mfrow=c(2,2))
+  for(symbol in symbols)
+  {
+    plot(chart_Series(get(symbol),name=symbol))
+  }
   #return nothing
   invisible();  
 }
---
+++ R/smoothplot.R
@@ -0,0 +1,15 @@
+#' Make a random plot
+#' 
+#' This function creates a random histogram plot.
+#' 
+#' @export
+#' @param n numer of random values 
+#' @param dist one of "normal" or "uniform".
+#library(stocks)
+#library(ggplot2)
+
+smoothplot <- function (ticker = "GOOG", from = "2013-01-01", to = Sys.time()) 
+{
+  mydata <- yahoodata(ticker, from, to)
+  qplot(Date, Close, data = mydata, geom = c("line", "smooth"))
+}
